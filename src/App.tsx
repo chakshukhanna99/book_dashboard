80945117968322d5e33163751f8e05878c0be764
Chakshu Khanna <chakshukhanna2110@gmail.com>
2024-04-26 22:04:03 +0530

    add test shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-
+import {Button} from '@/components/ui/button'
 function App() {
 
   return (
     <>
      <h1>Hello,Welcome to my book client dashboard</h1>
+     <Button variant={'destructive'}>Click Me</Button>
+     
     </>
   )
 }
