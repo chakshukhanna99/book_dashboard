02a3247ac7dc30fd3099480b4d070dff14df7860
Chakshu Khanna <chakshukhanna2110@gmail.com>
2024-04-26 21:17:07 +0530

    setup initial project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>Hello,Welcome to my book client dashboard</h1>
+    </>
+  )
+}
+
+export default App
