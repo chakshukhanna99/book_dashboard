6c5568b3650c01550b9dc19227d8b9fba3f3211b
Chakshu Khanna <chakshukhanna2110@gmail.com>
2024-04-28 02:09:32 +0530

    loading&error display login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,6 +6,7 @@ import { Label } from '@/components/ui/label';
 import {useMutation} from '@tanstack/react-query';
 import { Link,useNavigate } from 'react-router-dom';
 import { login } from '@/http/api';
+import { LoaderCircle } from 'lucide-react';
 const Login = () => {
     const emailRef = useRef<HTMLInputElement>(null);
     const passwordRef = useRef<HTMLInputElement>(null);
@@ -38,6 +39,7 @@ const Login = () => {
         <CardTitle className="text-2xl">Login</CardTitle>
         <CardDescription>
           Enter your email below to login to your account.
+          {mutation.isError&&<span className='text-red-500 text-sm'>{mutation.error.message}</span>}
         </CardDescription>
       </CardHeader>
       <CardContent className="grid gap-4">
@@ -51,7 +53,7 @@ const Login = () => {
         </div>
       </CardContent>
       <CardFooter>
-        <Button className="w-full" onClick={handleSign}>Sign in</Button>
+        <Button className="w-full" onClick={handleSign} disabled={mutation.isPending}>Sign in {mutation.isPending&&<span className='ml-2'><LoaderCircle className='animate-spin'/></span>} </Button>
         <div className="mt-4 text-center text-sm">
           Dont have an account?{" "}
           <Link to="/auth/register" className="underline">Login</Link>
