7e817a6003278a1b0a5b57b48f53d673c06332bf
Chakshu Khanna <chakshukhanna2110@gmail.com>
2024-04-28 13:13:53 +0530

    signup page

--- src/http/api.ts
+++ src/http/api.ts
@@ -10,3 +10,7 @@ const api = axios.create({
 export const login = async (data:{email:string;password:string}) =>{
     return api.post('api/users/login',data);
 };
+
+export const signup = async (data:{name:string,email:string;password:string}) =>{
+    return api.post('api/users/register',data);
+};
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,10 +1,40 @@
 import { Button } from '@/components/ui/button';
+import { useRef } from 'react'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { Link } from 'react-router-dom';
+import {useMutation} from '@tanstack/react-query';
+import { Link,useNavigate } from 'react-router-dom';
+import { signup } from '@/http/api';
+import { LoaderCircle } from 'lucide-react';
 
 const Register = () => {
+    const emailRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+    const nameRef=useRef<HTMLInputElement>(null);
+    const navigate = useNavigate();
+
+      // Mutations
+  const mutation = useMutation({
+    mutationFn: signup,
+    onSuccess: () => {
+      // Invalidate and refetch
+      console.log('login successfull');
+      navigate('/dashboard/home')
+    },
+  })
+  const signUp=()=>{
+    const email=emailRef.current?.value;
+    const password=passwordRef.current?.value;
+    const name = nameRef.current?.value;
+    //make server call
+    console.log("data",{email,password,name});
+    if(!email||!password||!name){
+        return alert("please enter valid email or password");
+    }
+
+    mutation.mutate({email,password,name});
+}
   return (
     <section className='flex justify-center h-screen items-center'>
 
@@ -13,17 +43,19 @@ const Register = () => {
         <CardTitle className="text-xl">Sign Up</CardTitle>
         <CardDescription>
           Enter your information to create an account
+          {mutation.isError&&<span className='text-red-500 text-sm'>{mutation.error.message}</span>}
         </CardDescription>
       </CardHeader>
       <CardContent>
         <div className="grid gap-4">
           <div className="grid gap-4">
-          <Label htmlFor="first-name">Full Name</Label>
-              <Input id="first-name" placeholder="Max" required />
+          <Label htmlFor="name">Full Name</Label>
+              <Input ref={nameRef} id="name" placeholder="Max" required />
           </div>
           <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
             <Input
+            ref={emailRef}
               id="email"
               type="email"
               placeholder="m@example.com"
@@ -32,20 +64,18 @@ const Register = () => {
           </div>
           <div className="grid gap-2">
             <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" />
+            <Input ref={passwordRef} id="password" type="password" />
           </div>
-          <Button type="submit" className="w-full">
-            Create an account
-          </Button>
+          <Button className="w-full" onClick={signUp} disabled={mutation.isPending}>Create an Account {mutation.isPending&&<span className='ml-2'><LoaderCircle className='animate-spin'/></span>} </Button>
           <Button variant="outline" className="w-full">
             Sign up with GitHub
           </Button>
         </div>
         <div className="mt-4 text-center text-sm">
           Already have an account?{" "}
-          <Link to="/auth/login" className="underline">Login</Link>
-
-           
+          <Link to='auth/login' className="underline">
+            Sign in
+          </Link>
         </div>
       </CardContent>
     </Card>
